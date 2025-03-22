069377c4722f0085b9f66ff074984a4d5e0edff8
Jafar Sadiq A <ajafarsadiq2002@gmail.com>
2025-03-22 18:11:45 +0530

    Initial Commit

--- main.py
+++ main.py
@@ -152,6 +152,7 @@ class WarehouseSimulator:
                 **Obstacles Hit:** `{count1 + count2}`  
                 **Penalty Incurred:** `{penalty1 + penalty2}`  
                 **Reward Earned:** `+10`
+                
                 **Path Taken:** `{full_path}`
                 """)
 
